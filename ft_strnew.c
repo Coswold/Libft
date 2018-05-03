6b161f7771b954452fd4555d2b1da609d262b693
Connor Oswold <cooswold@e1z3r2p20.42.us.org>
2018-05-03 13:12:03 -0700

    new

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: cooswold <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/26 14:56:28 by cooswold          #+#    #+#             */
-/*   Updated: 2018/04/29 16:19:17 by cooswold         ###   ########.fr       */
+/*   Updated: 2018/05/03 13:11:13 by cooswold         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,7 @@ char	*ft_strnew(size_t size)
 	i = 0;
 	if ((new = (char *)malloc(sizeof(char) * size + 1)) == NULL)
 		return (NULL);
-	new[size] = '\0';
-	while (new[i] != '\0')
-	{
-		new[i] = '\0';
-		i++;
-	}
+	while (i <= size)
+		new[i++] = '\0';
 	return (new);
 }
